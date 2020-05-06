c6c5b86ae4ae022d186670b6ef203d8c4ee9fbb2
Markandrew Terry <markandrew.terry@gmail.com>
2020-05-06 08:39:59 +0200

    Updates

---
+++ precache-manifest.0df55dab83b5ac7750560721f9ab2dc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd78c5387e067c5e8b2f9dda9c5f08a",
+    "url": "/rubyredmoon/cherrybluemoon.io/cherry/index.html"
+  },
+  {
+    "revision": "501cea59a97848a68f6f",
+    "url": "/rubyredmoon/cherrybluemoon.io/cherry/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4468a3b97300028f2c18",
+    "url": "/rubyredmoon/cherrybluemoon.io/cherry/static/js/2.155a1a15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rubyredmoon/cherrybluemoon.io/cherry/static/js/2.155a1a15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501cea59a97848a68f6f",
+    "url": "/rubyredmoon/cherrybluemoon.io/cherry/static/js/main.f8c5b6fe.chunk.js"
+  },
+  {
+    "revision": "8260aa4b7e241ad14587",
+    "url": "/rubyredmoon/cherrybluemoon.io/cherry/static/js/runtime-main.dba4b168.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rubyredmoon/cherrybluemoon.io/cherry/static/media/logo.5d5d9eef.svg"
+  }
+]);
